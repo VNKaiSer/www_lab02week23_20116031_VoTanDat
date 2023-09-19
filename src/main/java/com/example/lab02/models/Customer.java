837b1851b94d38828fba8316479edafc0b743dea
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2023-09-20 00:53:30 +0700

    final mapping

--- src/main/java/com/example/lab02/models/Customer.java
+++ src/main/java/com/example/lab02/models/Customer.java
@@ -3,7 +3,9 @@ package com.example.lab02.models;
 import com.example.lab02.enums.CustomerStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -12,6 +14,8 @@ import java.util.List;
 @NoArgsConstructor
 @Table(name = "customers")
 @AttributeOverride(name = "Id", column = @Column(name = "customer_id"))
+@Setter
+@Getter
 public class Customer extends Person{
     @Column(name = "status", columnDefinition = "enum('ACTIVE', 'IN_ACTIVE')")
     private CustomerStatus status;
