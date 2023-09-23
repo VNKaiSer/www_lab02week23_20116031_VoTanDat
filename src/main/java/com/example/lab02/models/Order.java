a45d28e494f138231c7428f220018f5f39248f0c
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2023-09-23 16:48:33 +0700

    done API order

--- src/main/java/com/example/lab02/models/Order.java
+++ src/main/java/com/example/lab02/models/Order.java
@@ -1,5 +1,7 @@
 package com.example.lab02.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,16 +24,21 @@ public class Order {
     @Id
     @Column(name = "order_id", nullable = false, length = 10)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("order_id")
     private long id;
 
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
+    @JsonProperty("customer_id")
     private Customer customer;
 
     @ManyToOne
     @JoinColumn(name = "employee_id", nullable = false)
+    @JsonProperty("employee_id")
     private Employee employee;
 
+    @JsonProperty("order_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime orderDate;
 
     @OneToMany(mappedBy = "order")
