3790f269a97ddc82c31eb3b57dc23891edc76a3d
VNKaiser <tandatvok16@gmail.com>
2023-11-11 10:04:19 +0700

    fix cors

---
+++ src/main/java/com/example/lab02/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.lab02.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CorsConfig implements ContainerResponseFilter {
+
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    }
+}
